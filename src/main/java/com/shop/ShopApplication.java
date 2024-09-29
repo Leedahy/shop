5c2b00f2094db6c7a031db1da8d87f909f85f93a
LeeDaHye <dh3529@naver.com>
2024-09-29 14:50:11 +0900

    first commit

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -1,9 +1,7 @@
 package com.shop;
 
-import org.springframework.aop.TargetSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/shop/controller/HelloController.java
+++ src/main/java/com/shop/controller/HelloController.java
@@ -1,12 +1,8 @@
 package com.shop.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
 
 @RestController
 //@Controller
